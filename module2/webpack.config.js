e3fe6247db3ec8ddd92cff7a3ebd15166097d3f0
Rustam <Rustamaaaa@bk.ru>
2022-02-26 16:07:11 +0300

    feat/собрать сборку на webpack

---
+++ module2/webpack.config.js
@@ -0,0 +1,65 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const filename = "[name].[contenthash]";
+const loader = MiniCssExtractPlugin.loader;
+
+module.exports = {
+  mode: "development",
+  context: resolve(__dirname, "src"),
+  resolve: {
+    extensions: [".js", ".ts", ".css"],
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  entry: { main: "./index.ts" },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: `${filename}.js`,
+  },
+  devServer: {
+    static: resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: `${filename}.css` }),
+    new ESLintPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+};
