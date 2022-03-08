dc6fd2348d149cff55978c86468576684bad3b37
Rustam <Rustamaaaa@bk.ru>
2022-03-08 14:29:44 +0300

    Добавил возможность реализовывать миксины. Добавил миксин text

--- module2/postcss.config.js
+++ module2/postcss.config.js
@@ -1,10 +1,18 @@
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 
+const { join } = require("path");
+
 module.exports = {
   plugins: [
-		autoprefixer, 
-		cssnano({ preset: "default" }), 
-		"postcss-nested"
-	],
+    autoprefixer,
+    cssnano({ preset: "default" }),
+    "postcss-nested",
+    [
+      "postcss-mixins",
+      {
+        mixinsDir: join(__dirname, "src/styles/mixins"),
+      },
+    ],
+  ],
 };
