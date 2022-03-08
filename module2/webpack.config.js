84710d6400cbba5dbe9167a3b1fd8ebc9222620f
Rustam <Rustamaaaa@bk.ru>
2022-03-08 14:19:10 +0300

    refactor: добавил алиас для папки public

--- module2/webpack.config.js
+++ module2/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     extensions: [".js", ".ts", ".css"],
     alias: {
       "@": resolve(__dirname, "src"),
+      "@@": resolve(__dirname, "public"),
     },
   },
   optimization: {
